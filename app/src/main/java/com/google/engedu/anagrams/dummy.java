2fc5b43e105235b201732e9afa703ca6e88d57b7
Pushan Paul <pushanweb@gmail.com>
2022-05-17 14:17:10 +0600

    dummy.java file created in branch b2

---
+++ app/src/main/java/com/google/engedu/anagrams/dummy.java
@@ -0,0 +1,17 @@
+package com.google.engedu.anagrams;
+
+public class dummy {
+    int b;
+
+    public dummy(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
